e1aacb41d1ad1f401581c7abd138a459cdbc26ba
demz41 <62586908+demz41@users.noreply.github.com>
2025-09-25 16:14:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,23 @@ elseif ($action === 'store') {
         $controller->store($_POST);
     }
 } 
-
+elseif ($action === 'edit') {
+    if (isset($_GET['id'])) {
+        $controller->edit($_GET['id']);
+    } else {
+        echo "User ID missing.";
+    }
+} 
+elseif ($action === 'update') {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
+        $controller->update($_POST['id'], $_POST);
+    }
+} 
+elseif ($action === 'delete') {
+    if (isset($_GET['id'])) {
+        $controller->delete($_GET['id']);
+    }
+} 
 else {
     echo "Page not found.";
 }
