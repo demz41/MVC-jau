4fa7a9640f42217cdb1419e6f8fed4bf0e9bda12
demz41 <62586908+demz41@users.noreply.github.com>
2025-09-25 16:14:35 +0800

    Update UserController.php

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController {
         $this->db = $database->getConnection();
         $this->user = new User($this->db);
     }
-
+  
     public function create() {
         include "views/user_create.php"; 
     }
@@ -20,13 +20,28 @@ class UserController {
         $result = $this->user->getAll();
         include "views/users_list.php";
     }
-    
+ 
     public function store($data) {
         $this->user->create($data['name'], $data['email']);
         header("Location: index.php");
         exit;
     }
 
+    public function edit($id) {
+        $userData = $this->user->getById($id);
+        include "views/user_edit.php";
+    }
+
+    public function update($id, $data) {
+        $this->user->update($id, $data['name'], $data['email']);
+        header("Location: index.php");
+        exit;
+    }
+
  
-    
+    public function delete($id) {
+        $this->user->delete($id);
+        header("Location: index.php");
+        exit;
+    }
 }
